adfb99c0ca58d640154465bb37d6bdc3b3c93c77
hernandezhernandezarturo19-source <hernandezhernandezarturo19@gmail.com>
2026-04-24 22:06:19 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -96,37 +96,3 @@ function register(){
 function logout() {
     window.location.href = "index.html";
   }
-  
-import { getFirestore, doc, getDoc, updateDoc } from "https://www.gstatic.com/firebasejs/8.10.1/firebase-firestore.js";
-
-const db = getFirestore();
-
-const userId = localStorage.getItem("userId");
-
-async function cargarDatos() {
-  const docRef = doc(db, "alumnos", userId);
-  const docSnap = await getDoc(docRef);
-
-  if (docSnap.exists()) {
-    const data = docSnap.data();
-
-    document.getElementById("nombre").value = data.nombre;
-    document.getElementById("edad").value = data.edad;
-    document.getElementById("curp").value = data.curp;
-    document.getElementById("grupo").value = data.grupo;
-  }
-}
-
-cargarDatos();
-async function guardarDatos() {
-  const docRef = doc(db, "alumnos", userId);
-
-  await updateDoc(docRef, {
-    nombre: document.getElementById("nombre").value,
-    edad: document.getElementById("edad").value,
-    curp: document.getElementById("curp").value,
-    grupo: document.getElementById("grupo").value
-  });
-
-  alert("Datos actualizados correctamente");
-}
