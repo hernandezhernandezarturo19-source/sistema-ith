d50277f362707e7c4f0ecb24941a66c88a2a183b
hernandezhernandezarturo19-source <hernandezhernandezarturo19@gmail.com>
2026-04-24 19:36:42 -0600

    Add files via upload

--- admin.js
+++ admin.js
@@ -1,4 +1,4 @@
-import { db } from "./firebase.js";
+import { db } from "firebase.js";
 import { collection, getDocs, deleteDoc, doc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 async function cargarAlumnos() {
