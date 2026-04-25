39eef4daadadfdfe456f1910ca979bde09f76522
hernandezhernandezarturo19-source <hernandezhernandezarturo19@gmail.com>
2026-04-24 21:20:33 -0600

    Add files via upload

--- dashboard.js
+++ dashboard.js
@@ -1,4 +1,4 @@
-import { db } from "firebase.js";
+import { db } from "./firebase.js";
 import { doc, getDoc, setDoc } from "https://www.gstatic.com/firebasejs/8.10.1/firebase-firestore.js";
 
 const userId = localStorage.getItem("userId");
