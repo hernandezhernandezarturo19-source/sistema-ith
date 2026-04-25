2d8db1f0c6b224870ad481a9e749ea222929dd9d
hernandezhernandezarturo19-source <hernandezhernandezarturo19@gmail.com>
2026-04-24 19:21:44 -0600

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,7 +1,3 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-
 const firebaseConfig = {
   apiKey: "AIzaSyDEKbWrNcb3OuSQ58mq6rPbZHkqaTod1Es",
   authDomain: "sistema-ith.firebaseapp.com",
@@ -11,6 +7,5 @@ const firebaseConfig = {
   appId: "1:262491381723:web:2dd5232ba5f1f1582c11fe"
 };
 
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export const auth = getAuth(app);
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
