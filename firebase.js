a7b7bfdd6bb6a5ac2ef42cba1761121c89117af8
hernandezhernandezarturo19-source <hernandezhernandezarturo19@gmail.com>
2026-04-24 22:17:51 -0600

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,4 +1,3 @@
-
 const firebaseConfig = {
   apiKey: "AIzaSyDEKbWrNcb3OuSQ58mq6rPbZHkqaTod1Es",
   authDomain: "sistema-ith.firebaseapp.com",
@@ -7,6 +6,5 @@ const firebaseConfig = {
   messagingSenderId: "262491381723",
   appId: "1:262491381723:web:2dd5232ba5f1f1582c11fe"
 };
-
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
