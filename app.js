83971a7baeb3f7ae72d75aafb425a3307b39e4dd
hernandezhernandezarturo19-source <hernandezhernandezarturo19@gmail.com>
2026-04-24 22:03:07 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ function register(){
     let c = control.value;
     let p = pass.value;
     
-    if(c==="admin" && p==="admin"){
+    if(c==="admin" && p==="12345"){
      window.location="admin.html";
      return;
     }
@@ -96,3 +96,37 @@ function register(){
 function logout() {
     window.location.href = "index.html";
   }
+  
+import { getFirestore, doc, getDoc, updateDoc } from "https://www.gstatic.com/firebasejs/8.10.1/firebase-firestore.js";
+
+const db = getFirestore();
+
+const userId = localStorage.getItem("userId");
+
+async function cargarDatos() {
+  const docRef = doc(db, "alumnos", userId);
+  const docSnap = await getDoc(docRef);
+
+  if (docSnap.exists()) {
+    const data = docSnap.data();
+
+    document.getElementById("nombre").value = data.nombre;
+    document.getElementById("edad").value = data.edad;
+    document.getElementById("curp").value = data.curp;
+    document.getElementById("grupo").value = data.grupo;
+  }
+}
+
+cargarDatos();
+async function guardarDatos() {
+  const docRef = doc(db, "alumnos", userId);
+
+  await updateDoc(docRef, {
+    nombre: document.getElementById("nombre").value,
+    edad: document.getElementById("edad").value,
+    curp: document.getElementById("curp").value,
+    grupo: document.getElementById("grupo").value
+  });
+
+  alert("Datos actualizados correctamente");
+}
